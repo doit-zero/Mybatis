7bfef2fb44ca1b7de47c3c23dc28da4c5d94db2e
doit-zero <131241458+doit-zero@users.noreply.github.com>
2024-01-25 11:06:34 +0900

    PathVariable 테스트

--- src/main/java/com/example/mybatis/Controller/UserController.java
+++ src/main/java/com/example/mybatis/Controller/UserController.java
@@ -2,10 +2,7 @@ package com.example.mybatis.Controller;
 
 import com.example.mybatis.Service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
@@ -18,8 +15,8 @@ public class UserController {
         this.userService = userService;
     }
 
-    @GetMapping("/user")
-    public String getUserById(@RequestParam Long id){
+    @GetMapping("/user/{id}")
+    public String getUserById(@PathVariable Long id){
         return userService.findUserById(id).toString();
     }
 
